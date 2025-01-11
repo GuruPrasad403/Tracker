ca21c7d8ee1e331e7668efe031a1464559bac8e9
guruprasad403 <guruprasas27@gmail.com>
2025-01-12 01:42:12 +0530

    basic change

--- validations/userBank.validate.js
+++ validations/userBank.validate.js
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 export const UserBankValidation = z.object({
     bankNames : z.object({
-        name : z.enum(["bank","wallet","savings"]),
+        name : z.enum(["Bank","Wallet","Savings"]),
         amount:z.number().gte(0)
     })
 })
